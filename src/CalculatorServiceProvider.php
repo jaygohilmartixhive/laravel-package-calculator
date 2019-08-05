08e0a4515bdf71065af512dfff45b928ec26f7e7
jaygohilmartixhive <53734985+jaygohilmartixhive@users.noreply.github.com>
2019-08-05 12:21:29 +0530

    Add files via upload

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Devdojo\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //Rgister Controller here
+        $this->app->make('Devdojo\Calculator\CalculatorController');
+        //Rgister View folder to acces blade file's here
+        $this->loadViewsFrom(__DIR__.'/views', 'calculator');
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+    }
+}
