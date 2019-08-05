9c23c078a0a950c77d622ad26c8476a6b6e5469c
jaygohilmartixhive <53734985+jaygohilmartixhive@users.noreply.github.com>
2019-08-05 19:25:02 +0530

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 
-Route::get('j/calculator', function(){
+Route::get('calculator', function(){
 	echo 'Hello from the calculator package!';
 });
-Route::get('j/add/{a}/{b}', 'Devjay\Calculator\CalculatorController@add');
-Route::get('j/subtract/{a}/{b}', 'Devjay\Calculator\CalculatorController@subtract');
+Route::get('add/{a}/{b}', 'Devjay\Calculator\CalculatorController@add');
+Route::get('subtract/{a}/{b}', 'Devjay\Calculator\CalculatorController@subtract');
