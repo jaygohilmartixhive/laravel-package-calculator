b9b9f749c82b39b4dfb9683245c73d284127f913
jaygohilmartixhive <53734985+jaygohilmartixhive@users.noreply.github.com>
2019-08-05 17:45:46 +0530

    Delete routes.php

--- devjay/calculator/src/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::get('j/calculator', function(){
-	echo 'Hello from the calculator package!';
-});
-Route::get('j/add/{a}/{b}', 'Devjay\Calculator\CalculatorController@add');
-Route::get('j/subtract/{a}/{b}', 'Devjay\Calculator\CalculatorController@subtract');
